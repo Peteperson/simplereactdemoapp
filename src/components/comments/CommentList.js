9a282af1d4300c1da1997259b78a2c55ed8faad4
peteperson <Peteperson_2000@yahoo.com>
2017-11-09 22:55:16 +0200

    Add block ui

--- src/components/comments/CommentList.js
+++ src/components/comments/CommentList.js
@@ -1,39 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types'
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
-import OverlayLoader from 'react-overlay-loading/lib/OverlayLoader';
+import BlockUi from 'react-block-ui';
 
 const selectRowProp = {
     mode: 'checkbox',
     bgColor: '#444444',
     hideSelectColumn: false,
     clickToSelect: true
-}; 
+};
 
 const options = {
-    clearSearch: true 
-}; 
+    clearSearch: true
+};
 
-const cellEditProp = { mode: 'dbclick' }; 
+const cellEditProp = { mode: 'dbclick' };
 
 const CommentList = ({ comments }) => {
     return (
-        <OverlayLoader
-            color={'red'}
-            loader="ScaleLoader"
-            text="Loading... Please wait!"
-            active={!(comments && comments.length > 0)}
-            backgroundColor={'gray'}
-            opacity=".8">
+        <BlockUi tag="div" blocking={!(comments && comments.length > 0)}>
             <BootstrapTable data={comments} pagination search exportCSV deleteRow insertRow
                 selectRow={selectRowProp} cellEdit={cellEditProp} options={options}>
                 <TableHeaderColumn isKey dataField="id" hidden>Comment ID</TableHeaderColumn>
                 <TableHeaderColumn dataField="name" width="250" dataSort={true}
                     tdStyle={{ whiteSpace: 'normal' }}>Name</TableHeaderColumn>
-                <TableHeaderColumn dataField="body" dataSort={true} 
+                <TableHeaderColumn dataField="body" dataSort={true}
                     tdStyle={{ whiteSpace: 'normal' }}>Message</TableHeaderColumn>
             </BootstrapTable>
-        </OverlayLoader>
+        </BlockUi>
     );
 };
 
