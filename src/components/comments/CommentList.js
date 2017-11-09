33990b11328ced5cc9452cbf5bb3d28c8453dc93
peteperson <peteperson_2000@yahoo.com>
2017-11-09 23:15:31 +0200

    Add block ui in commentslist

--- src/components/comments/CommentList.js
+++ src/components/comments/CommentList.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types'
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 import BlockUi from 'react-block-ui';
+import 'react-block-ui/style.css';
 
 const selectRowProp = {
     mode: 'checkbox',
@@ -18,16 +19,18 @@ const cellEditProp = { mode: 'dbclick' };
 
 const CommentList = ({ comments }) => {
     return (
-        <BlockUi tag="div" blocking={!(comments && comments.length > 0)}>
-            <BootstrapTable data={comments} pagination search exportCSV deleteRow insertRow
-                selectRow={selectRowProp} cellEdit={cellEditProp} options={options}>
-                <TableHeaderColumn isKey dataField="id" hidden>Comment ID</TableHeaderColumn>
-                <TableHeaderColumn dataField="name" width="250" dataSort={true}
-                    tdStyle={{ whiteSpace: 'normal' }}>Name</TableHeaderColumn>
-                <TableHeaderColumn dataField="body" dataSort={true}
-                    tdStyle={{ whiteSpace: 'normal' }}>Message</TableHeaderColumn>
-            </BootstrapTable>
-        </BlockUi>
+        <div>
+            <BlockUi tag="div" blocking={!(comments && comments.length > 0)}>
+                <BootstrapTable data={comments} pagination search exportCSV deleteRow insertRow
+                    selectRow={selectRowProp} cellEdit={cellEditProp} options={options}>
+                    <TableHeaderColumn isKey dataField="id" hidden>Comment ID</TableHeaderColumn>
+                    <TableHeaderColumn dataField="name" width="250" dataSort={true}
+                        tdStyle={{ whiteSpace: 'normal' }}>Name</TableHeaderColumn>
+                    <TableHeaderColumn dataField="body" dataSort={true}
+                        tdStyle={{ whiteSpace: 'normal' }}>Message</TableHeaderColumn>
+                </BootstrapTable>
+            </BlockUi>
+        </div>
     );
 };
 
