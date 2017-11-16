f20d0034185bc785fbdb6a44177d795ad44a7b5e
Panos Argoudelis <pargoudelis@eurobank.gr>
2017-11-16 16:20:14 +0200

    Replace normal select element with TypeAhead

--- src/components/common/SelectInput.js
+++ src/components/common/SelectInput.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types'
+import { Typeahead } from 'react-bootstrap-typeahead';
 
 const SelectInput = ({ name, label, onChange, defaultOption, value, error, options }) => {
   return (
-    <div className="form-group">
+    <div>
       <label htmlFor={name}>{label}</label>
-      <div className="field">
-        <select name={name} value={value} onChange={onChange} className="form-control">
-          {options.map((option) => {
-            return <option key={option.value} value={option.value}>{option.text}</option>;
-          })}
-        </select>
+      <div>
+        <Typeahead
+          labelKey="name"
+          onChange={(e) => {
+              if (options.length > 0 && e.length > 0)
+                onChange({ obj: options.find(x => x.id === e[0].id), name: 'authorId' });
+            }
+          }
+          multiple={false}
+          options={options}
+          selected={[options.find(x => x.id === value)]}
+          placeholder="Select Author" />
         {error && <div className="alert alert-danger">{error}</div>}
       </div>
     </div>
--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types'
-import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import * as courseActions from '../../actions/courseActions';
 import CourseForm from './CourseForm';
 import toastr from 'toastr';
 
-class ManageCoursePage  extends React.Component {
-  constructor(props, context){
+class ManageCoursePage extends React.Component {
+  constructor(props, context) {
     super(props, context);
     this.state = {
       course: Object.assign({}, this.props.course),
@@ -19,45 +19,52 @@ class ManageCoursePage  extends React.Component {
   }
 
   componentWillReceiveProps(nextProps) {
-    if(this.props.course.id !== nextProps.course.id){
-      this.setState({course: Object.assign({}, nextProps.course)});
+    if (this.props.course.id !== nextProps.course.id) {
+      this.setState({ course: Object.assign({}, nextProps.course) });
     }
   }
 
-  saveCourse(event){
+  saveCourse(event) {
     event.preventDefault();
-    this.setState({saving: true});
+    this.setState({ saving: true });
     this.props.actions.saveCourse(this.state.course)
       .then(() => this.redirect())
       .catch(error => {
-        this.setState({saving: false});
+        this.setState({ saving: false });
         toastr.error(error);
       });
   }
 
-  redirect(){
-    this.setState({saving: false});
+  redirect() {
+    this.setState({ saving: false });
     toastr.success('Course saved');
     this.context.router.push('/courses');
   }
 
   updateCourseState(event) {
-    const field = event.target.name;
+    let field, value;
+    if (event.target) {
+      field = event.target.name;
+      value = event.target.value;
+    }else{
+      field = event.name;
+      value = event.obj.id;
+    }
     let course = Object.assign({}, this.state.course);
-    course[field] = event.target.value;
-    return this.setState({course: course});
+    course[field] = value;
+    return this.setState({ course: course });
   }
 
-  render(){
+  render() {
     return (
       <CourseForm
-        allAuthors={this.props.authors}
+        allAuthors={this.props.authors.map(q => { return { id: q.value, name: q.text }; })}
         onChange={this.updateCourseState}
         onSave={this.saveCourse}
         course={this.state.course}
         errors={this.state.errors}
         saving={this.state.saving}
-        />
+      />
     );
   }
 }
@@ -72,16 +79,16 @@ ManageCoursePage.contextTypes = {
   router: PropTypes.object
 };
 
-function getCourseById(courses, id){
+function getCourseById(courses, id) {
   const course = courses.filter(course => course.id === id);
-  if(course.length) return course[0];
+  if (course.length) return course[0];
   return null;
 }
 
-function mapStateToProps(state, ownProps){
+function mapStateToProps(state, ownProps) {
   const courseId = ownProps.params.id; // taken from routes.js line 14 course/:id
-  let course = {id: '', watchHref: '', title: '', authorId: '', length: '', category: ''};
-  if(courseId && state.courses.length > 0){
+  let course = { id: '', watchHref: '', title: '', authorId: '', length: '', category: '' };
+  if (courseId && state.courses.length > 0) {
     course = getCourseById(state.courses, courseId);
   }
   const authorsFormattedForDropdown = state.authors.map(author => {
@@ -96,7 +103,7 @@ function mapStateToProps(state, ownProps){
   };
 }
 
-function mapDispatchToProps(dispatch){
+function mapDispatchToProps(dispatch) {
   return {
     actions: bindActionCreators(courseActions, dispatch)
   };
