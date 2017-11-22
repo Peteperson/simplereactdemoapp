db57c4252b26623bbd358c08b6e5fea14b182ad8
Panos Argoudelis <pargoudelis@eurobank.gr>
2017-11-22 13:51:48 +0200

    Add libraries for DatePicker and Icons

--- src/components/common/header.js
+++ src/components/common/header.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types'
 import { Nav, Navbar, NavDropdown, MenuItem, NavItem } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import logo from '../../resources/EurobankWin10.png';
+import { FaHome, FaBook, FaUser, FaComment, FaCalendar, 
+         FaShoppingBag, FaQuestion, FaPowerOff, FaLock } from 'react-icons/lib/fa'
 
 const Header = ({ loading, authenticated }) => {
   return (
@@ -16,27 +18,27 @@ const Header = ({ loading, authenticated }) => {
       <Navbar.Collapse>
         <Nav>
           <LinkContainer to="/home">
-            <NavItem eventKey={1}>Home</NavItem>
+            <NavItem eventKey={1}><FaHome/> Home</NavItem>
           </LinkContainer>
           <LinkContainer to="/courses" style={authenticated === 1  ? null : { display: 'none' }}>
-            <NavItem eventKey={2}>Courses</NavItem>
+            <NavItem eventKey={2}><FaBook /> Courses</NavItem>
           </LinkContainer>
           <LinkContainer to="/users" style={authenticated === 1 ? null : { display: 'none' }}>
-            <NavItem eventKey={3}>Users</NavItem>
+            <NavItem eventKey={3}><FaUser /> Users</NavItem>
           </LinkContainer>
           <LinkContainer to="/comments" style={authenticated === 1 ? null : { display: 'none' }}>
-            <NavItem eventKey={4}>Comments</NavItem>
+            <NavItem eventKey={4}><FaComment /> Comments</NavItem>
           </LinkContainer>
           <LinkContainer to="/schedules" style={authenticated === 1 ? null : { display: 'none' }}>
-            <NavItem eventKey={6}>Schedules</NavItem>
+            <NavItem eventKey={6}><FaCalendar /> Schedules</NavItem>
           </LinkContainer>
           <LinkContainer to="/actbranches" style={authenticated === 1 ? null : { display: 'none' }}>
-            <NavItem eventKey={7}>Active Brc</NavItem>
+            <NavItem eventKey={7}><FaShoppingBag /> Active Brc</NavItem>
           </LinkContainer>
           <NavDropdown eventKey={5} title="Help" id="basic-nav-dropdown"
             style={authenticated === 1 ? null : { display: 'none' }}>
             <LinkContainer to="/about">
-              <MenuItem eventKey={5.1}>About</MenuItem>
+              <MenuItem eventKey={5.1}><FaQuestion /> About</MenuItem>
             </LinkContainer>
             <LinkContainer to="/about">
               <MenuItem eventKey={5.2}>qwe</MenuItem>
@@ -60,10 +62,10 @@ const Header = ({ loading, authenticated }) => {
         </Nav>
         <Nav pullRight>
           {authenticated === 1 ? <LinkContainer to="/logoff">
-            <NavItem eventKey={6}>Log off</NavItem>
+            <NavItem eventKey={6}><FaPowerOff /> Log off</NavItem>
           </LinkContainer>
             : <LinkContainer to="/login">
-              <NavItem eventKey={7}>Login</NavItem>
+              <NavItem eventKey={7}><FaLock /> Login</NavItem>
             </LinkContainer>}
         </Nav>
       </Navbar.Collapse>
