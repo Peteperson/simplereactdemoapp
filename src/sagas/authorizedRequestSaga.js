e18bc3fd95b6ec4fe7e6e88aee834d76c0f10bbd
peteperson <peteperson_2000@yahoo.com>
2017-12-03 21:00:30 +0200

    if an authorized request fails, then we do not call load dictionaries

--- src/sagas/authorizedRequestSaga.js
+++ src/sagas/authorizedRequestSaga.js
@@ -5,10 +5,11 @@ import { authorizedRequest } from '../utility/restClient';
 import { loadDictionaries } from '../actions/dictActions';
 
 export function* authorizedRequestSaga() {
-    while (true) {
-        const req = yield take(CALL_REST_SERVICE);
-        yield put(loadDictionaries({ name: 'statuses', type: 'get', api: '/api/Views/ScheduleStatus' }));
-        const schedsList = yield call(authorizedRequest, req.requestInfo.type, req.requestInfo.api);
-        yield put(dataRecieved(schedsList));
-    }
+   while (true) {
+      const req = yield take(CALL_REST_SERVICE);
+      const response = yield call(authorizedRequest, req.requestInfo.type, req.requestInfo.api);
+      if(response)
+         yield put(loadDictionaries({ name: 'statuses', type: 'get', api: '/api/Views/ScheduleStatus' }));
+      yield put(dataRecieved(response));
+   }
 }
