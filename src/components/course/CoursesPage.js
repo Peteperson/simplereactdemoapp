3a63638bbdea7af22ed910e508e386a68314710f
peteperson <peteperson_2000@yahoo.com>
2017-12-08 23:20:04 +0200

    Put data into panel

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,59 +1,61 @@
 import React from 'react';
 import PropTypes from 'prop-types'
-import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Panel } from 'react-bootstrap'
 import * as courseActions from '../../actions/courseActions';
 import CourseList from './CourseList';
 import { browserHistory } from 'react-router';
 
 class CoursesPage extends React.Component {
-  constructor(props, context){
-    super(props, context);
-    this.redirectToAddCoursePage = this.redirectToAddCoursePage.bind(this);
-  }
+   constructor(props, context) {
+      super(props, context);
+      this.redirectToAddCoursePage = this.redirectToAddCoursePage.bind(this);
+   }
 
-  courseRow(course, index){
-    return (
-      <div key={index}>{course.title}</div>
+   courseRow(course, index) {
+      return (
+         <div key={index}>{course.title}</div>
       );
-  }
+   }
 
-  redirectToAddCoursePage(){
-    browserHistory.push('/course');
-  }
+   redirectToAddCoursePage() {
+      browserHistory.push('/course');
+   }
 
-  render() {
-    const {courses} = this.props;
+   render() {
+      const { courses } = this.props;
 
-    return (
-      <div>
-        <h1>Courses</h1>
-        <input type="submit"
-              value="Add course"
-              className="btn btn-primary"
-              onClick={this.redirectToAddCoursePage}/>
-        <CourseList courses={courses} />
-      </div>
-    );
-  }
+      return (
+         <div>
+            <Panel header="Courses" className="topOffset">
+               <input type="submit"
+                  value="Add course"
+                  className="btn btn-primary"
+                  onClick={this.redirectToAddCoursePage} />
+               <CourseList courses={courses} />
+            </Panel>
+         </div>
+      );
+   }
 }
 
 CoursesPage.propTypes = {
-  courses: PropTypes.array.isRequired,
-  actions: PropTypes.object.isRequired
+   courses: PropTypes.array.isRequired,
+   actions: PropTypes.object.isRequired
 };
 
-function mapStateToProps(state, ownProps){
-  //state.courses equals courses in line 2 of ...reducers/index.js
-  return {
-    courses: state.courses
-  };
+function mapStateToProps(state, ownProps) {
+   //state.courses equals courses in line 2 of ...reducers/index.js
+   return {
+      courses: state.courses
+   };
 }
 
-function mapDispatchToProps(dispatch){
-  return {
+function mapDispatchToProps(dispatch) {
+   return {
       actions: bindActionCreators(courseActions, dispatch)
-  };
+   };
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
